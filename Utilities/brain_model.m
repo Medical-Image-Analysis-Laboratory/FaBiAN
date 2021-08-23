4ed867769bbfe5caf69e274c36960f7cfe90e813
HeleneMIAL <helene.lajous@unil.ch>
2021-08-23 17:32:40 +0200

    Useful functions are located in the Utilities/ folder.

---
+++ Utilities/brain_model.m
@@ -0,0 +1,28 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  Function that reads the high-resolution segmented images of the fetal  %
+%  brain from which the simulated images will be derived.                 %
+%  Here, we consider high-resolution segmented images from Gholipour, A.  %
+%  et al. A normative spatiotemporal MRI atlas of the fetal brain for     %
+%  automatic segmentation and analysis of early brain growth. Scientific  %
+%  Reports 7, 1–13, DOI: 10.1038/s41598-017-00525-w (2017).               %
+%                                                                         %
+%                                                                         %
+%  Hélène Lajous, 2021-04-30                                              %
+%                                                                         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+function Fetal_Brain = brain_model(path, ...
+                                     GA)
+
+
+% Input check
+if nargin < 2
+    error('Missing input(s).');
+elseif nargin > 2
+    error('Too many inputs.');
+end
+
+
+% Load segmented HR images of the fetal brain at gestational age GA
+Fetal_Brain = niftiread(strcat(path, 'STA', sprintf('%02s', num2str(GA)), '_tissue.nii.gz'));
