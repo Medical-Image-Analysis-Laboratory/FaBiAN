cbad632d8fdc2ca8584ac0d075fec13e6b79684e
HeleneMIAL <helene.lajous@unil.ch>
2021-09-02 12:22:19 +0200

    Updated reference and copyright

--- Utilities/Resize_Volume.m
+++ Utilities/Resize_Volume.m
@@ -3,8 +3,13 @@
 %  desired size.                                                          %
 %                                                                         %
 %                                                                         %
-%  Christopher W. Roy, 2018-12-04                                         %
+%  (c) Christopher W. Roy, 2018-12-04                                     %
 %  fetal.xcmr@gmail.com                                                   %
+%  https://github.com/cwroy/Fetal-XCMR/                                   %
+%  Roy, C.W. et al. Fetal XCMR: a numerical phantom for fetal             %
+%  cardiovascular magnetic resonance imaging. Journal of Cardiovascular   %
+%  Magnetic Resonance 21, 29 (2019).                                      %
+%  https://doi.org/10.1186/s12968-019-0539-2                              %
 %                                                                         %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
