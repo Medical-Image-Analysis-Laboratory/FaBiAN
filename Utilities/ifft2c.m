a009115f9b996309556b7948f8bb4eccc8a77a6f
HeleneMIAL <helene.lajous@unil.ch>
2021-09-02 12:33:18 +0200

    Add function that performs 2D inverse Fourier transform

---
+++ Utilities/ifft2c.m
@@ -0,0 +1,21 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  Two-dimensional inverse Fourier transform                              %
+%                                                                         %
+%                                                                         %
+%  (c) Christopher W. Roy, 2018-12-04                                     %
+%  fetal.xcmr@gmail.com                                                   %
+%  https://github.com/cwroy/Fetal-XCMR/                                   %
+%  Roy, C.W. et al. Fetal XCMR: a numerical phantom for fetal             %
+%  cardiovascular magnetic resonance imaging. Journal of Cardiovascular   %
+%  Magnetic Resonance 21, 29 (2019).                                      %
+%  https://doi.org/10.1186/s12968-019-0539-2                              %
+%                                                                         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+function x = ifft2c(x)
+
+fctr = size(x,1)*size(x,2);        
+x = sqrt(fctr)*fftshift(ifft2(ifftshift(x)));
+
+end
